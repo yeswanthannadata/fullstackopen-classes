d8f1a723b8462b2373da1f37e3355a9a25a3d99b
Yeshu <yeswanth@flutura.com>
2022-07-08 22:24:38 +0530

    few changes

---
+++ mongo.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log(
+    "Please provide the password as the argument: node mongo.js <password> "
+  );
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://yeswanth:${password}@nodeexpress.nfcbg.mongodb.net/noteApp?retryWrites=true&w=majority`;
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+});
+
+const Note = mongoose.model("Note", noteSchema);
+
+mongoose.connect(url).then((result) => {
+  console.log("connected");
+
+  // const note = new Note({
+  //   content: "NodeJs is necessary",
+  //   date: new Date(),
+  //   important: false,
+  // });
+
+  // return note.save();
+  Note.find({}).then((notes) => {
+    notes.forEach((note) => console.log(note));
+    mongoose.connection.close();
+  });
+});
